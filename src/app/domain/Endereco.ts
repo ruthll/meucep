5e4452e5517b866dc06f9354581c832408d54f54
Ruth Leia de Lima <ruthlima019@gmail.com>
2019-07-23 22:04:02 -0300

    implementado estrutura de listagem de classe de dominio de endereço

---
+++ src/app/domain/Endereco.ts
@@ -0,0 +1,10 @@
+class Endereco{
+    rua:String;
+    cep:String;
+    numero:String;
+    bairro:String;
+    cidade:String;
+    estado:String;
+}
+
+    export default Endereco
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import Endereco from 'src/app/domain/Endereco'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,4 +9,15 @@ export class HomePage {
 
   constructor() {}
 
+  enderecos:Array<Object> =[]
+
+  ionViewDidEnter(){
+    let endereco1 = new Endereco()
+    endereco1.rua = 'Rua Cambara'
+    endereco1.bairro = 'Jardim Miray'
+    endereco1.cidade = 'Itaquaquecetuba'
+    endereco1.estado = 'SP'
+    endereco1.numero = '866'
+    this.enderecos.push(endereco1)
+  }
 }
